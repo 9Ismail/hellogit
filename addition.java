66738975d44a228ff17c48ac5a877688befa8ec9
mohd isamil <omohd9398@gmail.com>
2022-10-04 16:39:48 +0530

    new implementation

--- addition.java
+++ addition.java
@@ -3,6 +3,7 @@ public class addition {
     public static void main(String[] args) {
     int a=5;
     int b=6;
+    System.out.println("hello");
   
     System.out.println( "the add is " + (a+b));
    } 
